04ae59e8742edf2208ad17ab37c7bf984eaceea4
english <english@3328-MAC26.local>
2022-09-19 10:49:35 -0400

    implement logic

--- DogsAge/DogsAge/ViewController.swift
+++ DogsAge/DogsAge/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtAge: UITextField!
+    @IBOutlet weak var lblDogAge: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func calculateDogsAge(_ age: Int) -> Int {
+        return age * 7
+    }
 
+    @IBAction func txtAgeEditingChanged(_ sender: Any) {
+        lblDogAge.isHidden = true
+    }
+    
+    @IBAction func btnCheckInOutTouchUpInside(_ sender: Any) {
+        
+        if let age = Int(txtAge.text!) {
+            if (age >= 1 && age <= 20) {
+                let ageHuman = calculateDogsAge(age)
+                lblDogAge.text = "Your dog's age in human age is \(ageHuman)"
+                lblDogAge.isHidden = false
+            } else {
+                Toast.ok(view: self, title: "Oops!", message: "Please, enter a valid age between 1 and 20 years!", handler: nil)
+            }
+        } else {
+            Toast.ok(view: self, title: "Oops!", message: "Please, enter a valid number!", handler: nil)
+        }
+        
+    }
 }
 
